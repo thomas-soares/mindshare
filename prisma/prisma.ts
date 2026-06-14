ddf2b4005de2bb189bb6351dab99730cb3d8d35b
Thomas Soares <thomas__soares@hotmail.com>
2026-06-13 23:28:24 -0300

    fix: rename prisma export to prismaClient for clarity and consistency

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -2,6 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prismaClient = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+globalForPrisma.prisma = prismaClient;
